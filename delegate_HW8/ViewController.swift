5b600e8d864d564912c3336265ac3142f6c729e6
irving926 <irving926@gmail.com>
2016-08-04 15:38:58 +0800

    Initial Commit

---
+++ delegate_HW8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  delegate_HW8
+//
+//  Created by 周 修儀 on 2016/8/4.
+//  Copyright © 2016年 周 修儀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
